4ab8f3c6cd527867ba7d500bcdec04d7fb8f4c12
feildrixliemdra <feildrix.liemdra@ralali.com>
2020-04-10 18:40:19 +0700

    en: add text input in city_screen

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -18,3 +18,22 @@ const kButtonTextStyle = TextStyle(
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
 );
+
+const kWeatherApiKey = '69de4c4c0440e88baaa45abdafb717f9';
+
+const kTextInputDecurationStyle = InputDecoration(
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  filled: true,
+  fillColor: Colors.white,
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide.none,
+  ),
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+);
